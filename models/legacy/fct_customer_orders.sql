c935186e8070a66ddbb37d950e8630c88ab79bc3
Yolanda Yue <yolandayxfmoon@gmail.com>
2025-09-14 20:57:38 +0000

    use format tab to auto format the fct_ file to all lower case

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,68 +1,79 @@
 -- import cte
-with 
+with
 
-orders as (select *
-from {{ source('jaffle_shop', 'orders') }}), 
+    orders as (select * from {{ source("jaffle_shop", "orders") }}),
 
-customers as (select * from {{ source('jaffle_shop', 'customers') }}),
+    customers as (select * from {{ source("jaffle_shop", "customers") }}),
 
-payment as (select * from {{ source('stripe', 'payment') }}),
+    payment as (select * from {{ source("stripe", "payment") }}),
 
+    -- logical cte
+    paid_orders as (
+        select
+            orders.id as order_id,
+            orders.user_id as customer_id,
+            orders.order_date as order_placed_at,
+            orders.status as order_status,
+            p.total_amount_paid,
+            p.payment_finalized_date,
+            c.first_name as customer_first_name,
+            c.last_name as customer_last_name
+        from orders
+        left join
+            (
+                select
+                    orderid as order_id,
+                    max(created) as payment_finalized_date,
+                    sum(amount) / 100.0 as total_amount_paid
+                from payment
+                where status <> 'fail'
+                group by 1
+            ) p
+            on orders.id = p.order_id
+        left join customers c on orders.user_id = c.id
+    ),
 
--- logical cte
-paid_orders as (select Orders.ID as order_id,
-        Orders.USER_ID    as customer_id,
-        Orders.ORDER_DATE AS order_placed_at,
-            Orders.STATUS AS order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        C.FIRST_NAME    as customer_first_name,
-            C.LAST_NAME as customer_last_name
-    FROM orders
-    left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-from payment
-where STATUS <> 'fail'
-group by 1) p ON orders.ID = p.order_id
-left join customers C on orders.USER_ID = C.ID ),
-
-
-customer_orders as (select C.ID as customer_id
-        , min(ORDER_DATE) as first_order_date
-        , max(ORDER_DATE) as most_recent_order_date
-        , count(ORDERS.ID) AS number_of_orders
-    from customers C 
-    left join orders
-    on orders.USER_ID = C.ID 
-    group by 1),
-
-
--- final cte
-
-final as (select
-    p.*,
-    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-    CASE WHEN c.first_order_date = p.order_placed_at
-    THEN 'new'
-    ELSE 'return' END as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-    FROM paid_orders p
-    left join customer_orders as c USING (customer_id)
-    LEFT OUTER JOIN 
-    (
-            select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    customer_orders as (
+        select
+            c.id as customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(orders.id) as number_of_orders
+        from customers c
+        left join orders on orders.user_id = c.id
         group by 1
-        order by p.order_id
-    ) x on x.order_id = p.order_id
-    ORDER BY order_id)
+    ),
 
+    -- final cte
+    final as (
+        select
+            p.*,
+            row_number() over (order by p.order_id) as transaction_seq,
+            row_number() over (
+                partition by customer_id order by p.order_id
+            ) as customer_sales_seq,
+            case
+                when c.first_order_date = p.order_placed_at then 'new' else 'return'
+            end as nvsr,
+            x.clv_bad as customer_lifetime_value,
+            c.first_order_date as fdos
+        from paid_orders p
+        left join customer_orders as c using (customer_id)
+        left outer join
+            (
+                select p.order_id, sum(t2.total_amount_paid) as clv_bad
+                from paid_orders p
+                left join
+                    paid_orders t2
+                    on p.customer_id = t2.customer_id
+                    and p.order_id >= t2.order_id
+                group by 1
+                order by p.order_id
+            ) x
+            on x.order_id = p.order_id
+        order by order_id
+    )
 
 -- simple select
-
 select *
 from final
