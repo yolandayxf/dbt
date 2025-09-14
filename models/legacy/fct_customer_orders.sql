fa085616457cce02f5362b0f357f0d31367a7e23
Yolanda Yue <yolandayxfmoon@gmail.com>
2025-09-14 21:27:26 +0000

    changed the final cte back to p. and c. and the p. and c. here don't reference payments and customers

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -47,32 +47,33 @@ with
     ),
 
     -- final cte
+
     final as (
         select
-            payments.*,
-            row_number() over (order by payments.order_id) as transaction_seq,
+            p.*,
+            row_number() over (order by p.order_id) as transaction_seq,
             row_number() over (
-                partition by customer_id order by payments.order_id
+                partition by customer_id order by p.order_id
             ) as customer_sales_seq,
             case
-                when customers.first_order_date = payments.order_placed_at then 'new' else 'return'
+                when c.first_order_date = p.order_placed_at then 'new' else 'return'
             end as nvsr,
             x.clv_bad as customer_lifetime_value,
-            customers.first_order_date as fdos
+            c.first_order_date as fdos
         from paid_orders p
         left join customer_orders as c using (customer_id)
         left outer join
             (
-                select payments.order_id, sum(t2.total_amount_paid) as clv_bad
+                select p.order_id, sum(t2.total_amount_paid) as clv_bad
                 from paid_orders p
                 left join
                     paid_orders t2
-                    on payments.customer_id = t2.customer_id
-                    and payments.order_id >= t2.order_id
+                    on p.customer_id = t2.customer_id
+                    and p.order_id >= t2.order_id
                 group by 1
-                order by payments.order_id
+                order by p.order_id
             ) x
-            on x.order_id = payments.order_id
+            on x.order_id = p.order_id
         order by order_id
     )
 
