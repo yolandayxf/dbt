80c89a2d897af4522d2ec6b94f5d0173ba44ffec
Yolanda Yue <yolandayxfmoon@gmail.com>
2025-09-15 03:13:18 +0000

    change name for completed payments

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -6,15 +6,15 @@ with
 
     customers as (select * from {{ source("jaffle_shop", "customers") }}),
 
-    base_payment as (select * from {{ source("stripe", "payment") }}),
+    payment as (select * from {{ source("stripe", "payment") }}),
 
 -- logical cte
     
-    payments as (select
+    completed_payments as (select
                     orderid as order_id,
                     max(created) as payment_finalized_date,
                     sum(amount) / 100.0 as total_amount_paid
-                from base_payment
+                from payment
                 where status <> 'fail'
                 group by 1
     ),
@@ -25,12 +25,12 @@ with
             orders.user_id as customer_id,
             orders.order_date as order_placed_at,
             orders.status as order_status,
-            payments.total_amount_paid,
-            payments.payment_finalized_date,
+            completed_payments.total_amount_paid,
+            completed_payments.payment_finalized_date,
             customers.first_name as customer_first_name,
             customers.last_name as customer_last_name
         from orders 
-        left join payments on orders.id = payments.order_id
+        left join completed_payments on orders.id = completed_payments.order_id
         left join customers on orders.user_id = customers.id
     ),
 
