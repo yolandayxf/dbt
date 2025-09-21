b30d9e63471b43a340f604ad583546d9a6ad3825
Yolanda Yue <yolandayxfmoon@gmail.com>
2025-09-21 20:06:51 +0000

    build snapshot mock_orders

--- models/marts/intermediate/int_orders.sql
+++ models/marts/intermediate/int_orders.sql
@@ -2,36 +2,38 @@ with
 
 -- import cte
 
-    orders as (select * from {{ source("jaffle_shop", "orders") }}),
+    orders as (select * from {{ ref('stg_jaffle_shop__orders') }}),
 
-    customers as (select * from {{ source("jaffle_shop", "customers") }}),
+    customers as (select * from {{ ref('stg_jaffle_shop__customers') }}),
 
-    payment as (select * from {{ source("stripe", "payment") }}),
+    payment as (select * from {{ ref('stg_stripe__payments') }}),
 
 -- logical cte
     
     completed_payments as (select
-                    orderid as order_id,
-                    max(created) as payment_finalized_date,
-                    sum(amount) / 100.0 as total_amount_paid
+                    order_id,
+                    max(payment_created_at) as payment_finalized_date,
+                    sum(payment_amount) as total_amount_paid
                 from payment
-                where status <> 'fail'
+                where payment_status <> 'fail'
                 group by 1
     ),
 
     paid_orders as (
         select
-            orders.id as order_id,
-            orders.user_id as customer_id,
-            orders.order_date as order_placed_at,
-            orders.status as order_status,
+            orders.order_id,
+            orders.customer_id,
+            orders.order_placed_at,
+            orders.order_status,
+
             completed_payments.total_amount_paid,
             completed_payments.payment_finalized_date,
-            customers.first_name as customer_first_name,
-            customers.last_name as customer_last_name
+
+            customers.customer_first_name,
+            customers.customer_last_name
         from orders 
-        left join completed_payments on orders.id = completed_payments.order_id
-        left join customers on orders.user_id = customers.id
+        left join completed_payments on orders.order_id = completed_payments.order_id
+        left join customers on orders.customer_id = customers.customer_id
     )
 
-    select * from paid orders
+    select * from paid_orders
